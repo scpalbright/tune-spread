95315aee7ae199e29fc2483a340b94b875efffdd
fasvesta <as.fotini@gmail.com>
2023-04-25 11:13:22 +0200

    added virtual environment file

--- input_parameters.py
+++ input_parameters.py
@@ -1,17 +1,17 @@
 #input parameters
-intensity       =      83e10
+intensity       =      25e10
 #bunchLength_ns =       150 #4 sigma in ns
 #bunchLength_m	=	    10.67 #1 sigma in m
-emittance_x     =       1.58e-6
-emittance_y     =       1.1e-6
-dpp_rms         =       1.1e-3
+emittance_x     =       2.1e-6
+emittance_y     =       1.7e-6
+dpp_rms         =       1.85e-3
 #ro             =       # classical radius : default proton change for ions accordingly
-bF              =       0.31 # None for Gaussian
-Qh		=	6.2    # to estimate detuning and plot
-Qv		=	6.24    # to estimate detuning and plot
-plot_range  =   [[5.9,6.3],[5.9,6.3]]   # range in Qh & Qv for the plot
+bF              =       0.24 # None for Gaussian
+Qh		=	4.17    # to estimate detuning and plot
+Qv		=	4.23    # to estimate detuning and plot
+plot_range  =   [[3.9,4.3],[3.9,4.3]]   # range in Qh & Qv for the plot
 plot_order  =   4   # order of resonances to plot
-periodicity =   50  # periodicity of ring for the colorcode of the plot
+periodicity =   16  # periodicity of ring for the colorcode of the plot
 figure		=	'figure.png'
 twiss_file  =   'twiss_files/twiss_PSB'
 #b  =   # relativistic beta  :  if desired energy different than in twiss file
---
+++ virtual_environment_for_tune_spread.sh
@@ -0,0 +1,3 @@
+python3.10 -m venv "venv"
+. venv/bin/activate
+pip install numpy==1.23.1 sympy==1.10.1 matplotlib==3.5.1 dill==0.3.4
